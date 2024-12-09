bfeb7cbe6378a58082ef8aeadb81cc04e287396e
cheeyung <thyecheeyung@gmail.com>
2024-12-09 12:09:43 +0800

    Added netlify SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    srcDir: 'src',
+  srcDir: 'src',
+  adapter: netlify(),
 });
